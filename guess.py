679ae7162263d2f29f92fc4cf2691eeb93314f4b
SunTalk <infernal56727@gmail.com>
2019-02-20 17:02:35 +0800

    add guess number

---
+++ guess.py
@@ -0,0 +1,23 @@
+import random
+
+upper = 99
+lower = 0
+count = 0
+ans = random.randint(0,99)
+
+while 1 :
+	print( "Guess in(",lower,"," ,upper,")" )
+	test = int(input())
+	count = count + 1
+
+	if test < lower or test > upper:
+		print( "Your guess is not in range" )
+	elif test < ans :
+		lower = test
+	elif test > ans :
+		upper = test;
+	elif test == ans :
+		print("You are right")
+		break
+
+print("You guess",count,"times")
